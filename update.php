d76bf3f313dda7e88a47cc3771710348e54cb58b
Martin Aarhof <martin.aarhof@gmail.com>
2019-01-03 23:33:44 +0100

    update

--- update.php
+++ update.php
@@ -5,6 +5,4 @@ if (random_int(0, 1) === 1) {
     $search = '/<footer>(.*)<\/footer>/';
     $replace = '<footer>Sidst kontrolleret: ' . (new \DateTime())->format('d-m-Y k\l H:i') . '</footer>';
     file_put_contents(__DIR__ . '/index.html', preg_replace($search, $replace, $text));
-    `git commit -am "Updated"`;
-    `git push`;
 }
